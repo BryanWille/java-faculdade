dffb25f2e772ca93d884ae7e1c3efe422efa58cb
Bryan <bryanelliw@gmail.com>
2021-05-17 14:57:09 -0300

    conteudo 9

--- Conteúdo 9/src/ex1c9.java
+++ Conteúdo 9/src/ex1c9.java
@@ -1,2 +1,10 @@
 public class ex1c9 {
+	public static void main(String[] args) {
+		double numero = 0, somador = 0;
+		for (int c = 37; c > 0; c--) {
+			numero += 1;
+			somador += c * (c + 1) / numero;
+		}
+		System.out.println(somador);
+	}
 }
