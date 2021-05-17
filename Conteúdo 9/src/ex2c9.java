e0b689924c3c3323dffcb3b1d7c37c44146c9cd1
Bryan <bryanelliw@gmail.com>
2021-05-17 15:00:07 -0300

    commit 9

--- Conteúdo 9/src/ex2c9.java
+++
@@ -1,9 +0,0 @@
-public class ex2c9 {
-    public static void main(String[] args){
-        double div = 0;
-        for (int c = 0; 100 >= c; c++){
-            div += c / (Math.pow(c, 2) + (c - 1));
-        }
-        System.out.println(div);
-    }
-}
