ee2a462153d9d8278ef5116b08ecbe2018a1990c
Bryan <bryanelliw@gmail.com>
2021-05-17 14:50:28 -0300

    conteúdo 6

--- Conteúdo 8/src/ex19c8.java
+++ Conteúdo 8/src/ex19c8.java
@@ -1,6 +1,6 @@
 public class ex19c8 {
     public static void main(String[] args) {
-        System.out.println("Isso é 2 eleavado a n, é bastante usado em computação");
+        System.out.println("Isto é 2 eleavado a n, é bastante usado em computação");
         for (int c = 0; 11 >= c; c++) {
             System.out.println(Math.pow(2, c));
         }
