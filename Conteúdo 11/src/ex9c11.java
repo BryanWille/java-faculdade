60d465f661f69ac50e11bb1b91baaa12c6060f71
Bryan <bryanelliw@gmail.com>
2021-05-18 12:57:15 -0300

    conteúdo 11

--- Conteúdo 11/src/ex9c11.java
+++ Conteúdo 11/src/ex9c11.java
@@ -2,47 +2,49 @@ import javax.swing.JOptionPane;
 
 public class ex9c11 {
 
+    private String maisNovo;
+
     public static void main(String[] args) {
         int ano, setor, quantGer = 0, quantAdm = 0, quantPes = 0, quantObr = 0, quantAdmDez = 0,
-                reg, maiorRegistro = 0, menorRegistro = 0, menorAno = 0, maiorAno = 0, contador = 0;
+                menorAno = 0, menorRg = 0, maiorAno = 0, contador = 0, rg, maiorRg = 0;
         String nome = "", msg = "Registro Funcional", maiorSetor, maisVelho = "", maisNovo = "";
         while (!nome.equalsIgnoreCase("FIM")) {
             nome = JOptionPane.showInputDialog(null,
-                    "Digite o nome do funcionário: ('FIM' para encerrar",
+                    "Digite o nome do funcionário: ('FIM') para encerrar",
                     msg, JOptionPane.QUESTION_MESSAGE);
             if (nome.equalsIgnoreCase("FIM")) {
                 break;
             }
             contador++;
             int registro = Integer.parseInt(JOptionPane.showInputDialog(null,
-                    "Digite o registro de " + nome + ", :",
+                    "Digite o registro de " + nome + ":",
                     msg, JOptionPane.QUESTION_MESSAGE));
             ano = registro / 10000;
             setor = (registro / 1000) % 10;
-            reg = registro % 1000;
+            rg = registro % 1000;
             if (ano > 14 || ano < 10) {
-                JOptionPane.showMessageDialog(null, "REGISTRO INVÁLIDO ",
+                JOptionPane.showMessageDialog(null, "REGISTRO INVÁLIDO",
                         msg, JOptionPane.QUESTION_MESSAGE);
                 continue;
             }
             if (contador == 1) {
-                maiorAno = ano;
-                menorAno = ano;
-                maiorRegistro = reg;
-                menorRegistro = reg;
-            }
-            if (menorAno >= ano) {
-                menorAno = ano;
-                if (menorRegistro > reg) {
-                    menorRegistro = reg;
-                    maisNovo = nome;
-                }
-            }
-            if (ano >= maiorAno) {
-                maiorAno = ano;
-                if (reg > maiorRegistro) {
-                    maiorRegistro = reg;
-                    maisVelho = nome;
+                maiorAno = menorAno = ano;
+                maiorRg = menorRg = rg;
+            } else {
+                if (menorAno >= ano) {
+                    menorAno = ano;
+                    if (menorRg > rg) {
+                        maisNovo = nome;
+                        menorRg = rg;
+                    }
+                    if (ano >= maiorAno) {
+                        maiorAno = ano;
+                        maisVelho = nome;
+                        if (rg > maiorRg) {
+                            maisVelho = nome;
+                            maiorRg = rg;
+                        }
+                    }
                 }
             }
             switch (setor) {
---
+++ Conteúdo 11/src/teste.java
@@ -0,0 +1,6 @@
+public class teste {
+    public static void main(String[] args){
+        System.out.println(102798 % 1000);
+
+    }
+}
