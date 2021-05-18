025e47b882a11b4bb18775da7272a7ba52924a1d
Bryan <bryanelliw@gmail.com>
2021-05-18 18:26:33 -0300

    feito ex12c11

--- Conteúdo 11/src/ex10c11.java
+++ Conteúdo 11/src/ex10c11.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 public class ex10c11 {
     public static void main(String[] args) {
-        int repet, cont = 0;
+        int repet, cont = 0, aux;
         Scanner keyb = new Scanner(System.in);
         System.out.println("Quantas repetições você quer? ");
         repet = keyb.nextInt();
         while (cont != repet) {
             cont++;
-            System.out.print("emge ");
+            aux = cont;
+            while(aux != 0){
+                aux --;
+                System.out.print("EMGE ");
+            }
+            System.out.println("");
         }
-
     }
 }
