2288e0467995cf6ebd5aec38bc62be4f01bb489d
BryanWille <84272231+BryanWille@users.noreply.github.com>
2021-05-22 21:40:59 -0300

    ex8c12

--- Conteúdo 12/src/ex7c12.java
+++ Conteúdo 12/src/ex7c12.java
@@ -2,10 +2,26 @@ import javax.swing.JOptionPane;
 
 public class ex7c12 {
     public static void main(String[] args) {
-        String notaNome[][] = new String[50][1], msg = "Notas! ";
+        String nome[] = new String[3], rank[] = new String[3], msg = "Notas! ";
         JOptionPane.showMessageDialog(null, "Vamos te dar a relação dos 50 alunos");
-        for(int cont = 0; notaNome.length > cont; cont++){
-            JOptionPane.showInputDialog(null, "Digite o nome: ");
+        for (int cont = 0; nome.length > cont; cont++) {
+            nome[cont] = JOptionPane.showInputDialog(null,
+                    "Digite o nome: ", msg, JOptionPane.QUESTION_MESSAGE);
+            int nota = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "Qual a nota do aluno(a) " + nome[cont], msg, JOptionPane.QUESTION_MESSAGE));
+            if (nota <= 30) {
+                rank[cont] = "D";
+            } else if (nota <= 60) {
+                rank[cont] = "C";
+            } else if (nota <= 80) {
+                rank[cont] = "B";
+            } else {
+                rank[cont] = "A";
+            }
+        }
+        for (int cont = 0; cont < nome.length; cont++) {
+            JOptionPane.showMessageDialog(null, nome[cont] + " - "
+                    + rank[cont], msg, JOptionPane.INFORMATION_MESSAGE);
         }
     }
 }
---
+++ Conteúdo 12/src/ex9c12.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class ex9c12 {
+    public static void main(String[] args) {
+
+    }
+}
