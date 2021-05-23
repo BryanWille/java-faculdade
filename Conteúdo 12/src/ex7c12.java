5fba070735ce23611b8c36117e6eb85f23e8e38f
Bryan <bryanelliw@gmail.com>
2021-05-23 09:14:35 -0300

    ex7c12 corrigido com matriz e ex9c12 feito

--- Conteúdo 12/src/ex7c12.java
+++ Conteúdo 12/src/ex7c12.java
@@ -2,26 +2,28 @@ import javax.swing.JOptionPane;
 
 public class ex7c12 {
     public static void main(String[] args) {
-        String nome[] = new String[3], rank[] = new String[3], msg = "Notas! ";
+        String nomeNota[][] = new String[50][2], msg = "Notas! ";
         JOptionPane.showMessageDialog(null, "Vamos te dar a relação dos 50 alunos");
-        for (int cont = 0; nome.length > cont; cont++) {
-            nome[cont] = JOptionPane.showInputDialog(null,
-                    "Digite o nome: ", msg, JOptionPane.QUESTION_MESSAGE);
+        for (int cont = 0; nomeNota.length > cont; cont++) {
+            nomeNota[cont][0] = JOptionPane.showInputDialog(null,
+                    "Digite o nome do aluno: ", msg, JOptionPane.QUESTION_MESSAGE);
             int nota = Integer.parseInt(JOptionPane.showInputDialog(null,
-                    "Qual a nota do aluno(a) " + nome[cont], msg, JOptionPane.QUESTION_MESSAGE));
+                    "Qual a nota do aluno(a) " + nomeNota[cont][0], msg, JOptionPane.QUESTION_MESSAGE));
             if (nota <= 30) {
-                rank[cont] = "D";
+                nomeNota[cont][1] = "D";
             } else if (nota <= 60) {
-                rank[cont] = "C";
+                nomeNota[cont][1] = "C";
             } else if (nota <= 80) {
-                rank[cont] = "B";
+                nomeNota[cont][1] = "B";
             } else {
-                rank[cont] = "A";
+                nomeNota[cont][1] = "A";
             }
         }
-        for (int cont = 0; cont < nome.length; cont++) {
-            JOptionPane.showMessageDialog(null, nome[cont] + " - "
-                    + rank[cont], msg, JOptionPane.INFORMATION_MESSAGE);
+        for (int cont = 0; cont < nomeNota.length; cont++) {
+            for (int contador = 0; nomeNota[contador].length > contador; contador++) {
+                JOptionPane.showMessageDialog(null, nomeNota[cont][contador],
+                        msg, JOptionPane.INFORMATION_MESSAGE);
+            }
         }
     }
 }
