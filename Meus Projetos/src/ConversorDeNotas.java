a2805689044652cc801b093184120bc0ba159dc7
Bryan <bryanelliw@gmail.com>
2021-05-25 22:53:20 -0300

    Feito um conversor de notas do site virtual piano em java!

---
+++ Meus Projetos/src/ConversorDeNotas.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ConversorDeNotas {
+    public static void main(String[] args) {
+        int contador = 0;
+        String musica, trad[][] = new String[61][2];
+        Scanner tradutor = new Scanner(ConversorDeNotas.class.getResourceAsStream("notas.txt"));
+        Scanner simb = new Scanner(ConversorDeNotas.class.getResourceAsStream("numeros.txt"));
+        Scanner keyb = new Scanner(System.in);
+        System.out.println("Fale a nota que quer traduzir: ");
+        musica = keyb.nextLine();
+        musica = musica.replace("", " ").replace("[", " [")
+                .replace("]", "] ");
+        String notas[] = musica.split(" ");
+        while (tradutor.hasNextLine()) {
+            trad[contador][0] = simb.nextLine();
+            trad[contador][1] = tradutor.nextLine();
+            contador++;
+        }
+        for (int conta = 0; notas.length > conta; conta++) {
+            for (int cont = 0; (contador - 1) > cont; cont++) {
+                if (notas[conta].equals(trad[cont][0])) {
+                    notas[conta] = trad[cont][1];
+                }
+            }
+        }
+        for (int conta = 0; notas.length > conta; conta++) {
+            System.out.print(notas[conta] + " ");
+        }
+    }
+}
---
+++ Meus Projetos/src/fatiar.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class fatiar {
+    public static void main(String[] args) {
+        String teste;
+        int cont = 1;
+        teste = "s";
+        System.out.println(cont + teste);
+        /*Scanner txt = new Scanner(fatiar.class.getResourceAsStream("notas.txt"));
+        while (txt.hasNextLine()) {
+            teste = txt.nextLine();
+            System.out.println(teste.subSequence(5, 10));
+            System.out.println(teste.replace("'", ""));
+            cont++;
+        }
+        System.out.println(cont);
+        Scanner txte = new Scanner(fatiar.class.getResourceAsStream("numeros.txt"));
+        while(txte.hasNextLine()){
+            teste = txte.nextLine();
+            teste = teste.replace("'" ,"");
+            System.out.println(teste.subSequence(1, 2));
+
+        } */
+    }
+}
