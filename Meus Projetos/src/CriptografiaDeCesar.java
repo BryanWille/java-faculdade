cb6f9d823a2afc7b583be1bfd8f60b2674410aca
Bryan <bryanelliw@gmail.com>
2021-05-26 14:49:01 -0300

    Criptografia de César em java

--- Meus Projetos/src/CriptografiaDeCesar.java
+++ Meus Projetos/src/CriptografiaDeCesar.java
@@ -9,19 +9,10 @@ import javax.swing.JOptionPane;
 
 public class CriptografiaDeCesar {
     public static void main(String[] args) {
-
-        // Declaração de Variavéis!
-
         String botoes[] = {"Criptografar", "Descriptografar"}, crypt = "",
                 msg = "Criptografia de César", alpha[][] = new String[26][2];
         int opcao = 0;
-
-        //Aqui estamos pegando o Arquivo txt do nosso alfabeto!
-
         Scanner alfabeto = new Scanner(CriptografiaDeCesar.class.getResourceAsStream("alfabeto.txt"));
-
-        //Aqui estamos pegando o Arquivo txt da cifra de César!
-
         Scanner cesar = new Scanner(CriptografiaDeCesar.class.getResourceAsStream("alfabeto_cesar.txt"));
         JOptionPane.showMessageDialog(null, "Vamos Criptografar ou Descriptografar" +
                 " a Criptografia de César!", msg, JOptionPane.INFORMATION_MESSAGE);
@@ -33,57 +24,38 @@ public class CriptografiaDeCesar {
             alpha[contador][1] = cesar.nextLine();
         }
 
-        //Aqui vamos fazer as criptografias até o usuário pedir para parar
+        //Repetições, até o usuário encerrar
 
         do {
 
-            //Usuário nos dá a mensagem que quer criptografar ou descriptografar
+            //Entrada de dados
 
             crypt = JOptionPane.showInputDialog(null,
                     "Digite a sua mensagem: (Para sair digite /fechar)", msg, JOptionPane.QUESTION_MESSAGE);
-
-            //Se o usúario quiser fechar o programa, só usa /fechar
-
             if (crypt.equalsIgnoreCase("/fechar")) {
                 JOptionPane.showMessageDialog(null, "Ok :( \nFechando o Programa!",
                         msg, JOptionPane.INFORMATION_MESSAGE);
                 break;
-
             }
-
-            // Adicionado os espaços para não dar erro na troca de letras!
-
-            crypt = crypt.replace("", " ");
-
-            //Aqui estamos fazendo a Array da mensagem!
-
-            String mensagem[] = crypt.split(" ");
-
-            //Usuário seleciona se quer criptografar ou descriptografar!
-
+            crypt = crypt.replace("", " ");  // Adicionado os espaços para não dar erro na troca de letras!
+            String mensagem[] = crypt.split(" "); //Aqui estamos fazendo a Array da mensagem!
             opcao = JOptionPane.showOptionDialog(null, "O que você quer fazer " +
                             "com essa mensagem? ", msg, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
                     null, botoes, botoes[0]);
 
-            //Usando switch para o que o usuário selecionou!
+            //Seleção da escolha do usuário
 
             switch (opcao) {
 
-                //Se for 0 ou a opção "Criptografar", então vamos criptografar a mensagem que ele nos passou!
+                //Se for 0 é a opção "Criptografar"!
 
                 case 0:
-
                     //Temos dois for, o primeiro vai nos dar todas as posições da array da mensagem a ser criptografada!
-
                     for (int contador = 0; mensagem.length > contador; contador++) {
-
                         //O segundo for, vai dar as posições da matriz do alfabeto!
-
                         for (int cont = 0; alpha.length > cont; cont++) {
-
                             //Quando achar a palavra no alfabeto igual a do usuário, então ele vai passar
                             //a palavra na cifra de César equivalente, e fechar com o break!
-
                             if (mensagem[contador].equalsIgnoreCase(alpha[cont][0])) {
                                 mensagem[contador] = alpha[cont][1];
                                 break;
@@ -92,18 +64,14 @@ public class CriptografiaDeCesar {
                     }
                     break;
 
-                // Se for 1 ou a opção "Descriptografar", vamos descriptografar a mensagem que ele nos passou!
+                // Se for 1 é a opção "Descriptografar"!
 
                 case 1:
-
                     //Os for de descriptografia tem a mesma função de criptografia, o que muda é o if dentro do for
-
                     for (int contador = 0; mensagem.length > contador; contador++) {
                         for (int cont = 0; alpha.length > cont; cont++) {
-
                             //Agora vamos buscar a mensagem informada pelo usuáriu na cifra de César
                             //E depois a sua equivalente no alfabeto, e fechar o for quando acha-lá
-
                             if (mensagem[contador].equalsIgnoreCase(alpha[cont][1])) {
                                 mensagem[contador] = alpha[cont][0];
                                 break;
@@ -113,21 +81,17 @@ public class CriptografiaDeCesar {
                     break;
             }
 
-            //Aqui vamos imprimir agora a mensagem de acordo com input e output do usuário
+            //Saída de Dados
 
             for (int contador = 0; mensagem.length > contador; contador++) {
                 System.out.print(mensagem[contador] + " ");
             }
-
-            //Para separar essa da próxima mensagem Criptofrada!
-
             System.out.println("\n");
         } while (crypt.equalsIgnoreCase("/fechar"));
         cesar.close();
         alfabeto.close();
 
         //Fim, obrigado por ler!
-
 
     }
 }
