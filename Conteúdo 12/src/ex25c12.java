c4cdf9e1d91d0b36da658ea231f8fc743c972332
elliwx <bryanelliw@gmail.com>
2021-06-06 14:54:05 -0300

    conteúdo 12 finalizado!

--- Conteúdo 12/src/ex25c12.java
+++ Conteúdo 12/src/ex25c12.java
@@ -1,26 +1,20 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class ex25c12 {
     public static void main(String[] args) {
         String[] time = new String[20];
         int[] posicao = new int[20];
         int bolhaInt, conta = 0;
-        String bolhaStr, timePesq, status = "";
-        Scanner txt = new Scanner(ex25c12.class.getResourceAsStream("times.txt"));
-        while (txt.hasNextLine()) {
-            time[conta] = txt.nextLine();
-            posicao[conta] = Integer.parseInt(txt.nextLine());
-            conta++;
-        }
+        String bolhaStr, status = "", pesquisa;
+        boolean encontrado = false;
         Scanner keyb = new Scanner(System.in);
-        /*for (int cont = 0; cont < time.length; cont++) {
+        for (int cont = 0; cont < time.length; cont++) {
             System.out.println("Digite o nome do time: ");
             time[cont] = keyb.nextLine();
             System.out.println("Digite a posição do " + time[cont] + ": ");
             posicao[cont] = keyb.nextInt();
             keyb.nextLine();
-        } */
+        }
         for (int cont = 0; cont < time.length; cont++) {
             for (int aux = 0; aux < time.length - 1; aux++) {
                 if (posicao[aux] < posicao[aux + 1]) {
@@ -38,20 +32,25 @@ public class ex25c12 {
             System.out.println(time[cont] + " --- " + posicao[cont]);
         }
         System.out.println("Qual time você deseja procurar? ");
-        timePesq = keyb.nextLine();
-        int pos = Arrays.binarySearch(time, timePesq);
-        System.out.println(pos);
-        if (pos < 0) {
-            System.out.println("Time não encontrado!");
-        } else {
-            if (posicao[pos] >= 0 && posicao[pos] <= 4) {
-                status = "na Copa libertadores";
-            } else if (posicao[pos] <= 12) {
-                status = "na Copa Sul-Americana";
-            } else {
-                status = "Rebaixada";
+        pesquisa = keyb.nextLine();
+        for (int cont = 0; cont < time.length; cont++) {
+            if (pesquisa.equalsIgnoreCase(time[cont])) {
+                if (cont >= 0 && cont <= 3) {
+                    status = "na Copa libertadores";
+                } else if (cont <= 11) {
+                    status = "na Copa Sul-Americana";
+                } else if (cont >= 16) {
+                    status = "Rebaixado(a)";
+                } else {
+                    status = "Mediocre!";
+                }
+                System.out.printf("%s está %s, na posição %d!", time[cont], status, (cont + 1));
+                encontrado = true;
             }
-            System.out.printf("%s  está %s", time, status);
         }
+        if (!encontrado) {
+            System.out.println("Time não encontrao");
+        }
+        keyb.close();
     }
 }
