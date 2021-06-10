98583ee7625a4bf3a843dac47521c4b2cc89e93f
Bryan <bryanelliw@gmail.com>
2021-06-10 19:54:34 -0300

    terminando exercicios do modulo 13

--- Conteúdo 13/src/ex6c13.java
+++ Conteúdo 13/src/ex6c13.java
@@ -7,16 +7,24 @@ public class ex6c13 {
         Scanner keyb = new Scanner(System.in);
         System.out.println("Digite o sobrenome: ");
         sobrenome = keyb.nextLine();
-        for (int cont = 1; 4 >= cont; cont ++){
-            System.out.println(cont +". Digite o nome: ");
+        for (int cont = 1; 100 >= cont; cont++) {
+            System.out.println(cont + ". Digite o nome: ");
             nome = keyb.nextLine();
+            /*  Método com Vetor.
             String[] sobrPessoa = nome.split(" ");
             index = sobrPessoa.length -1;
             if (sobrPessoa[index].equalsIgnoreCase(sobrenome)){
                 quant ++;
+            } */
+            if (nome.substring(nome.length() - sobrenome.length()).equalsIgnoreCase(sobrenome)) {
+                quant++;
             }
         }
-        System.out.printf("\nFim, %d pessoas com o sobrenome %s!", quant, sobrenome);
-        keyb.close();
+        if (quant == 0) {
+            System.out.printf("Não teve ninguem na lista com o sobrenome %s :(", sobrenome);
+        } else {
+            System.out.printf("\nFim, %d pessoas com o sobrenome %s!", quant, sobrenome);
+            keyb.close();
+        }
     }
 }
