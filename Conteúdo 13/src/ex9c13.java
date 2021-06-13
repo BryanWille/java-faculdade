1362c6c6759468bfaaa908174be8c6a8c09a1d49
bryanwille <bryanelliw@gmail.com>
2021-06-13 13:29:35 -0300

    terminando conteudo 14

--- Conteúdo 13/src/ex9c13.java
+++ Conteúdo 13/src/ex9c13.java
@@ -14,24 +14,22 @@ public class ex9c13 {
         System.out.println("Programa encerrado!");
         keyb.close();
     }
-    public static String capitalize(String frases){
+
+    public static String capitalize(String frases) {
         int index;                  //Criação do index
         frases += " ";              //Frase ganha espaço no final
         String maisIgual = "";      //Vou dividir a frase em palavras e capitalizar elas
-        for (int cont = 0; cont < frases.length()-2; cont++) { //Contador vai até antes do espaço adicionado
+        for (int cont = 0; cont < frases.length() - 2; cont++) { //Contador vai até antes do espaço adicionado
             index = frases.indexOf(" "); //Indexamos o espaço atual
-            if(cont == 0){
-                maisIgual = frases.substring(cont, cont+1).toUpperCase() +
-                        frases.substring(cont+1, index);                             //Primeira palavra tem primeira letra maiuscula
-            } else if(frases.charAt(cont) == ' '){
-                maisIgual += " " + frases.substring(index+1, index+2).toUpperCase(); //Espaço para separar palavras, a primeira posição depois do espaço vira maiuscula
+            if (cont == 0) {
+                maisIgual = frases.substring(cont, cont + 1).toUpperCase() +
+                        frases.substring(cont + 1, index);                             //Primeira palavra tem primeira letra maiuscula
+            } else if (frases.charAt(cont) == ' ') {
+                maisIgual += " " + frases.substring(index + 1, index + 2).toUpperCase(); //Espaço para separar palavras, a primeira posição depois do espaço vira maiuscula
                 frases = frases.replaceFirst(" ", "-");             //Trocamos espaço para traço, pois o index só mostra o primeiro espaço.
-                maisIgual += frases.substring(index+2, frases.indexOf(" ")).toLowerCase(); //agora adicionamos o resto da palavra. onde começa no index+2 e termina no prox index.
+                maisIgual += frases.substring(index + 2, frases.indexOf(" ")).toLowerCase(); //agora adicionamos o resto da palavra. onde começa no index+2 e termina no prox index.
             }
         }
-        Aula Algoritmo e estrutura de Dados
-
-
         return maisIgual;
     }
 }
---
+++ Conteúdo 14/src/ex1c14.java
@@ -0,0 +1,5 @@
+public class ex1c14 {
+    public static void main(String[] args) {
+
+    }
+}
