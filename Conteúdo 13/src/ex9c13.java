cfa80564a0bbe8214c12c2ed79eb314b4903af14
BryanWille <84272231+BryanWille@users.noreply.github.com>
2021-06-12 22:51:25 -0300

    Exercícios do módulo 13 terminados!

--- Conteúdo 13/src/ex9c13.java
+++ Conteúdo 13/src/ex9c13.java
@@ -1,14 +1,17 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ex9c13 {
     public static void main(String[] args) {
-        String frases, frase = "";
+        String frases;
         Scanner keyb = new Scanner(System.in);
         do {
-            System.out.println("Digite uma frase ('fim' para encerrar: ");
+            System.out.println("Digite uma frase ('fim' para encerrar): ");
             frases = keyb.nextLine();
 
-        } while (!frase.equalsIgnoreCase("fim"));
+            }
+            System.out.println(frases);
+        } while (!frases.equalsIgnoreCase("fim"));
         System.out.println("fim");
         keyb.close();
     }
