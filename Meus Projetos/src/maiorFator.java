9f16a2b2c67ce7f9cdfa621cc97730bfe6acef52
BryanWille <84272231+BryanWille@users.noreply.github.com>
2021-06-21 22:16:18 -0300

    exercícios do project euler

---
+++ Meus Projetos/src/maiorFator.java
@@ -0,0 +1,12 @@
+public class maiorFator {
+    public static void main(String[] args) {
+        for (int numero = 1; 1000 > numero; numero += 2) {
+            int verificar = 0;
+            for (int contador = 0; numero > contador; contador++) {
+                if (numero % contador == 0) {
+                    verificar ++;
+                }
+            }
+        }
+    }
+}
