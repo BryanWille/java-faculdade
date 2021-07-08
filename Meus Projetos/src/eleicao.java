7386f4c0024bcc437b0f7b88f24f2ea3ba4125cd
bryanwille <bryanelliw@gmail.com>
2021-07-08 11:24:13 -0300

    Indo pro Windows

--- Meus Projetos/src/eleicao.java
+++ Meus Projetos/src/eleicao.java
@@ -6,16 +6,24 @@ public class eleicao {
         System.out.println("Digite em quem você vota: ");
         System.out.println("[0]nulo \n[1]Beltrano \n[2]Ciclano \n[3]Fulano");
         primeiroTurno(beltrano, ciclano, fulano, nulo);
+        int[] votos = primeiroTurno(beltrano, ciclano, fulano, nulo);
+        for(int cont = 0; cont < votos.length; cont++){
+            for(int aux = 0; aux < votos.length; aux++){
+                if(votos[cont] == votos[aux]){
+                    System.out.println("Tivemos um empate");
+                }
+            }
+        }
     }
     public static int[] primeiroTurno(int contBel, int contCic, int contFul, int contNul){
+        boolean votou;
         int voto;
         int[] resultados = new int[4];
         Scanner keyb = new Scanner(System.in);
         for(int cont = 1; cont <= 10; cont++){
-            boolean votou = true;
             do{
-                System.out.println("\n \n \n " +cont);
-                System.out.print("\nSeu voto >>> ");
+                votou = true;
+                System.out.print("\n" +cont +". Seu voto >>> ");
                 voto = keyb.nextInt();
                 switch(voto){
                     case 0:
