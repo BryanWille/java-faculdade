3ebed3060aab3324234aec0c4e3e32eda6a1ae51
Bryan <bryanelliw@gmail.com>
2021-07-19 20:59:47 -0300

    Refatorando

---
+++ BOOTCAMP POO/src/Person.java
@@ -0,0 +1,19 @@
+public class Company {
+    private String nome;
+
+    public Integer calcularImposto(){
+
+    }
+
+    public Integer calcularISS(){
+
+    }
+
+    public Integer calacularICMS(){
+
+    }
+
+    public Integer calcularPis(){
+
+    }
+}
