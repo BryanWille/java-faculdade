54aff4e36614bcf129b99f6464d03ad13fa29a63
Bryan <bryanelliw@gmail.com>
2021-07-22 20:28:40 -0300

    aula 2 poo java

---
+++ CursoEmVideo/src/aula03/Editar.java
@@ -0,0 +1,14 @@
+package aula03;
+
+public class Editar {
+    String title;
+    int releaseHour;
+
+    void release(){
+        if(releaseHour - 14 < 4){
+            System.out.println("You have to edit this right now!" +this.title);
+        } else {
+            System.out.println("Be careful with release hour" +this.title);
+        }
+    }
+}
