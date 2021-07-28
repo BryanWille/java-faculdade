fc1ce240ba11d9c5f090848ea3aea66267dec58e
bryanwille <bryanelliw@gmail.com>
2021-07-28 13:05:35 -0300

    Fazendo um exercício de celular com programação em objeto e com caixas de diálogos do JOptionPane

---
+++ CursoEmVideo/src/meutestecelular/MyPhone.java
@@ -0,0 +1,9 @@
+package meutestecelular;
+
+public class MyPhone {
+    public static void main(String[] args) {
+
+        Xiaomi bryan = new Xiaomi("brjuc4b3");
+        bryan.mainMenu();
+    }
+}
