4d30a81a491084b763a53af943155d909c363f47
bryanwille <bryanwille@github.com>
2021-08-02 11:50:54 -0300

    praticando poo com livro

--- CursoEmVideo/src/aula09/Livro.java
+++ CursoEmVideo/src/aula09/Livro.java
@@ -7,18 +7,16 @@ public class Livro implements Publicacao {
     private Pessoa leitor;
 
 
-    public Livro(String titulo, String autor, int totPaginas, int pagAtual, boolean aberto, Pessoa leitor){
-        this.setAberto(aberto);
+    public Livro(String titulo, String autor, int totPaginas,  Pessoa leitor){
         this.setAutor(autor);
         this.setLeitor(leitor);
         this.setTitulo(titulo);
         this.setTotPaginas(totPaginas);
-        this.setPagAtual(pagAtual);
     }
 
 
     public void detalhes(){
-        System.out.println("Olá, " +this.getLeitor() +" !");
+        System.out.println("Olá, " +this.getLeitor().getNome() +"!");
         if(this.isAberto()){
             System.out.println("-------------------------------");
             System.out.println("Autor: " +this.getAutor());
@@ -64,9 +62,11 @@ public class Livro implements Publicacao {
 
     @Override
     public void avancarPag() {
-        if(getPagAtual() < getTotPaginas()){
+        if(getPagAtual() < getTotPaginas() && this.isAberto()){
             this.setPagAtual(this.getPagAtual() + 1);
             System.out.println("Avançando uma página! agora ná pág: " +this.getPagAtual());
+        } else if (!this.isAberto()){
+            System.out.println("Livro fechado, abra-o antes");
         } else {
             System.out.println("Já terminamos de ler o livro");
         }
@@ -74,55 +74,57 @@ public class Livro implements Publicacao {
 
     @Override
     public void voltarPag() {
-        if(this.getPagAtual() != 1){
+        if(this.getPagAtual() != 1 && this.isAberto()){
             System.out.println("Voltando uma página!");
             this.setPagAtual(this.getPagAtual() - 1);
             System.out.println("Agora estamos ná pagina: " +this.getPagAtual());
+        } else if (!this.isAberto()){
+            System.out.println("Livro fechado, abra-o antes");
         } else {
-            System.out.println("Não podemos voltar o livro estamos na primeira página!");
+            System.out.println("Já terminamos de ler o livro");
         }
     }
 
 
-    public String getTitulo() {
+    private String getTitulo() {
         return titulo;
     }
-    public void setTitulo(String titulo) {
+    private void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
-    public String getAutor() {
+    private String getAutor() {
         return autor;
     }
-    public void setAutor(String autor) {
+    private void setAutor(String autor) {
         this.autor = autor;
     }
 
-    public Pessoa getLeitor() {
+    private Pessoa getLeitor() {
         return this.leitor;
     }
-    public void setLeitor(Pessoa leitor) {
+    private void setLeitor(Pessoa leitor) {
         this.leitor = leitor;
     }
 
-    public int getTotPaginas() {
+    private int getTotPaginas() {
         return totPaginas;
     }
-    public void setTotPaginas(int totPaginas) {
+    private void setTotPaginas(int totPaginas) {
         this.totPaginas = totPaginas;
     }
 
-    public int getPagAtual() {
+    private int getPagAtual() {
         return pagAtual;
     }
-    public void setPagAtual(int pagAtual) {
+    private void setPagAtual(int pagAtual) {
         this.pagAtual = pagAtual;
     }
 
-    public boolean isAberto() {
+    private boolean isAberto() {
         return aberto;
     }
-    public void setAberto(boolean aberto) {
+    private void setAberto(boolean aberto) {
         this.aberto = aberto;
     }
 }
