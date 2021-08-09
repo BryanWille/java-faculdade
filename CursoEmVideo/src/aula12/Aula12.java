8e000629303be5ec213e4f03f96c0f5bad9419db
Bryan Wille <bryanelliw@gmail.com>
2021-08-09 10:30:17 -0300

    Aula de Polimorfismo: Override e metódos iguais

---
+++ CursoEmVideo/src/aula12/Aula12.java
@@ -0,0 +1,13 @@
+package aula12;
+
+public class Aula12 {
+    public static void main(String[] args){
+
+
+        Canguru teste = new Canguru();
+        Cachorro k = new Cachorro();
+        teste.locomover();
+        k.emitirSom();
+        teste.emitirSom();
+    }
+}
