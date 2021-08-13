265a459d355430d16b045646f0415d339847e044
Bryan Wille <bryanelliw@gmail.com>
2021-08-13 11:43:08 -0300

    Lista 0: Exercícios

--- CursoEmVideo/src/meutestecelular/MyPhone.java
+++ CursoEmVideo/src/meutestecelular/MyPhone.java
@@ -3,7 +3,7 @@ package meutestecelular;
 public class MyPhone {
     public static void main(String[] args) {
 
-        Xiaomi bryan = new Xiaomi("brjuc4b3");
+        Xiaomi bryan = new Xiaomi("coxinha123");
         bryan.mainMenu();
     }
 }
