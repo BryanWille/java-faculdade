aa0264fb3a93d146e15dd729cccad962e3cb2591
BryanWille <bryanelliw@gmail.com>
2021-08-26 10:05:56 -0300

    começando recursividade

---
+++ AED2/src/Recursividade/recursividade.java
@@ -0,0 +1,12 @@
+package Recursividade;
+import java.util.Scanner;
+
+public class recursividade {
+    private static Scanner keyb;
+    public static void main(String[] args){
+        keyb = new Scanner(System.in);
+        System.out.println("Digite o seu número fatorial: ");
+        int fatorial = keyb.nextByte();
+
+    }
+}
