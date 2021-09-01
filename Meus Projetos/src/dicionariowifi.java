10aedc860c295501a76993abd493373f0878c0ee
BryanWille <84272231+BryanWille@users.noreply.github.com>
2021-09-01 08:43:47 -0300

    Fazendo alguns projetos

---
+++ Meus Projetos/src/dicionariowifi.java
@@ -0,0 +1,9 @@
+public class dicionariowifi {
+    public static void main(String[] args){
+        String num;
+        for(int cont = 10000000; cont <= 99999999; cont++){
+            num = "9" + String.valueOf(cont);
+            System.out.println(num);
+        }
+    }
+}
