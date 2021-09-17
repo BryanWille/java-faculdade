c46a7f52df74a62f3ce2ebf9150c7cfefe938683
BryanWille <bryanelliw@gmail.com>
2021-09-17 11:43:31 -0300

    Começando os exercicios de ordenaçao

--- AED2/src/Ordenacao/Ordenacao.java
+++ AED2/src/Ordenacao/Ordenacao.java
@@ -4,17 +4,86 @@ import java.util.Random;
 
 public class Ordenacao {
 
+	public Ordenacao() {
 
-    public int[] gerarVetor(int tamanhoVet, int digitos) {
-        Random random = new Random();
-        return random.ints(0, digitos).limit(tamanhoVet).toArray();
-    }
+	}
 
-    public String exibirVetor (int[] vetor) {
-        String exibir = "";
-        for(int i = 0; i <= vetor.length; i++) {
-            exibir += i +"\n";
-        }
-        return exibir;
-    }
+	public int[] geradorAleatorio(int tamanhoVet, int digitos) {
+		Random random = new Random();
+		return random.ints(0, digitos).limit(tamanhoVet).toArray();
+	}
+
+	public String exibirVetor(int[] vetor) {
+		String exibir = "";
+		for (int i = 0; i < vetor.length; i++) {
+			exibir += vetor[i] + " ";
+		}
+		return exibir;
+	}
+
+	public int[] bubbleSort(int[] vetor) {
+		int temp;
+		for(int i = 0; i < vetor.length; i++) {
+			for(int j = 0; j < (vetor.length-1); j++) {
+				if(vetor[j] < vetor[j+1]) {
+					temp = vetor[j];
+					vetor[j] = vetor[j+1];
+					vetor[j+1] = temp;
+				}
+			}
+		}
+		return vetor;
+	}
+
+	public int[] bubbleSort_Decrescente(int[] vetor) {
+		int temp;
+		for(int i = 0; i < vetor.length; i++) {
+			for(int j = 0; j < (vetor.length-1); j++) {
+				if(vetor[j] < vetor[j+1]) {
+					temp = vetor[j];
+					vetor[j] = vetor[j+1];
+					vetor[j+1] = temp;
+				}
+			}
+		}
+		return vetor;
+	}
+	
+	
+	public int[] insertionSort(int[] vetor) {
+		int chave, j;
+		for(int i = 1; i < vetor.length; i++) {
+			chave = vetor[i];
+			j = i - 1;
+			while (j >= 0 && vetor[j] > chave) {
+				vetor[j+1] = vetor[j];
+				j = j -1;
+			}
+			vetor[j+1] = chave;
+		}
+		return(vetor);
+	}
+	
+	public int[] insertionSort_Decrescente(int[] vetor) {
+		int chave, j;
+		for(int i = 1; i < vetor.length; i++) {
+			chave = vetor[i];
+			j = i - 1;
+			while (j >= 0 && vetor[j] < chave) {
+				vetor[j+1] = vetor[j];
+				j = j -1;
+			}
+			vetor[j+1] = chave;
+		}
+		return(vetor);
+	}
+	 
+	public int[] quickSort(int[] vetor) {
+
+	}
+
+	/*
+	public int[] mergeSort(int[] vetor) {
+
+	} */
 }
--- AED2/src/Ordenacao/Ordenacao_main.java
+++ AED2/src/Ordenacao/Ordenacao_main.java
@@ -2,12 +2,15 @@ package Ordenacao;
 
 public class Ordenacao_main {
 
-    public static void main(String[] args) {
-        Ordenacao ord = new Ordenacao();
-
-        int[] teste = ord.gerarVetor(100, 5);
-
-        System.out.println(ord.exibirVetor(teste));
-
-    }
+	private static Ordenacao ord;
+	
+	public static void main(String[] args) {
+		ord = new Ordenacao();
+		
+		int[] numeros = ord.geradorAleatorio(20, 99);
+		
+		System.out.println("Vetor: " +ord.exibirVetor(numeros));
+		numeros = ord.bubbleSort(numeros);
+		System.out.println("Bubble Sort: " +ord.exibirVetor(numeros));
+	}
 }
