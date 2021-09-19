4714fc8f67553dfa4f46d8b0595dd25b2f45103e
BryanWille <bryanelliw@gmail.com>
2021-09-19 13:46:21 -0300

    Entendendo a Recursão e o Quickas

--- AED2/src/Ordenacao/Ordenacao.java
+++ AED2/src/Ordenacao/Ordenacao.java
@@ -4,86 +4,119 @@ import java.util.Random;
 
 public class Ordenacao {
 
-	public Ordenacao() {
+    public Ordenacao() {
 
-	}
+    }
 
-	public int[] geradorAleatorio(int tamanhoVet, int digitos) {
-		Random random = new Random();
-		return random.ints(0, digitos).limit(tamanhoVet).toArray();
-	}
+    public int[] geradorAleatorio(int tamanhoVet, int digitos) {
+        Random random = new Random();
+        return random.ints(0, digitos).limit(tamanhoVet).toArray();
+    }
 
-	public String exibirVetor(int[] vetor) {
-		String exibir = "";
-		for (int i = 0; i < vetor.length; i++) {
-			exibir += vetor[i] + " ";
-		}
-		return exibir;
-	}
+    public String exibirVetor(int[] vetor) {
+        String exibir = "";
+        for (int i = 0; i < vetor.length; i++) {
+            exibir += vetor[i] + " ";
+        }
+        return exibir;
+    }
 
-	public int[] bubbleSort(int[] vetor) {
-		int temp;
-		for(int i = 0; i < vetor.length; i++) {
-			for(int j = 0; j < (vetor.length-1); j++) {
-				if(vetor[j] < vetor[j+1]) {
-					temp = vetor[j];
-					vetor[j] = vetor[j+1];
-					vetor[j+1] = temp;
-				}
-			}
-		}
-		return vetor;
-	}
+    public int[] bubbleSort(int[] vetor) {
+        int temp;
+        for (int i = 0; i < vetor.length; i++) {
+            for (int j = 0; j < (vetor.length - 1); j++) {
+                if (vetor[j] < vetor[j + 1]) {
+                    temp = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = temp;
+                }
+            }
+        }
+        return vetor;
+    }
 
-	public int[] bubbleSort_Decrescente(int[] vetor) {
-		int temp;
-		for(int i = 0; i < vetor.length; i++) {
-			for(int j = 0; j < (vetor.length-1); j++) {
-				if(vetor[j] < vetor[j+1]) {
-					temp = vetor[j];
-					vetor[j] = vetor[j+1];
-					vetor[j+1] = temp;
-				}
-			}
-		}
-		return vetor;
-	}
-	
-	
-	public int[] insertionSort(int[] vetor) {
-		int chave, j;
-		for(int i = 1; i < vetor.length; i++) {
-			chave = vetor[i];
-			j = i - 1;
-			while (j >= 0 && vetor[j] > chave) {
-				vetor[j+1] = vetor[j];
-				j = j -1;
-			}
-			vetor[j+1] = chave;
-		}
-		return(vetor);
-	}
-	
-	public int[] insertionSort_Decrescente(int[] vetor) {
-		int chave, j;
-		for(int i = 1; i < vetor.length; i++) {
-			chave = vetor[i];
-			j = i - 1;
-			while (j >= 0 && vetor[j] < chave) {
-				vetor[j+1] = vetor[j];
-				j = j -1;
-			}
-			vetor[j+1] = chave;
-		}
-		return(vetor);
-	}
-	 
-	public int[] quickSort(int[] vetor) {
+    public int[] bubbleSort_Decrescente(int[] vetor) {
+        int temp;
+        for (int i = 0; i < vetor.length; i++) {
+            for (int j = 0; j < (vetor.length - 1); j++) {
+                if (vetor[j] < vetor[j + 1]) {
+                    temp = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = temp;
+                }
+            }
+        }
+        return vetor;
+    }
 
-	}
+    public int[] insertionSort(int[] v) {
+        int chave, j;
+        for(int i = 1; i < v.length; i++) {
+            chave = v[i];
+            j = i - 1;
+            while(j >= 0 && v[j] > chave) {
+                v[j+1] = v[j];
+                j -= 1;
+            }
+            v[j+1] = chave;
+        }
+        return v;
+    }
 
-	/*
-	public int[] mergeSort(int[] vetor) {
+    public int[] insertionSort_Decrescente(int[] vetor) {
+        int chave, j;
+        for (int i = 1; i < vetor.length; i++) {
+            chave = vetor[i];
+            j = i - 1;
+            while (j >= 0 && vetor[j] < chave) {
+                vetor[j + 1] = vetor[j];
+                j = j - 1;
+            }
+            vetor[j + 1] = chave;
+        }
+        return (vetor);
+    }
 
-	} */
+    public int[] quickSort(int[] vetor, int inicio, int fim) {
+        if(inicio < fim ) {
+            int sep = separar(vetor, inicio, fim);
+            quickSort(vetor, inicio, sep - 1);
+            quickSort(vetor, sep + 1, fim);
+        }
+        return vetor;
+    }
+
+    private int separar(int[] vetor, int inicio, int fim) {
+        int pivo = vetor[inicio];
+        int f = fim;
+        int i = inicio + 1;
+        while(i < f) {
+            if(vetor[i] < pivo) {
+                i ++;
+            } else if (pivo < vetor[f]) {
+                f --;
+            } else {
+                int troca = vetor[f];
+                vetor[f] = vetor[i];
+                vetor[i] = troca;
+                i ++;
+                f --;
+            }
+
+        }
+        vetor[inicio] = vetor[f];
+        vetor[f] = pivo;
+
+        return f;
+
+    }
+
+    /*
+     * }
+     *
+     *
+     * public int[] mergeSort(int[] vetor) {
+     *
+     * }
+     */
 }
--- AED2/src/Ordenacao/Ordenacao_main.java
+++ AED2/src/Ordenacao/Ordenacao_main.java
@@ -3,14 +3,18 @@ package Ordenacao;
 public class Ordenacao_main {
 
 	private static Ordenacao ord;
-	
+
 	public static void main(String[] args) {
 		ord = new Ordenacao();
-		
-		int[] numeros = ord.geradorAleatorio(20, 99);
-		
-		System.out.println("Vetor: " +ord.exibirVetor(numeros));
-		numeros = ord.bubbleSort(numeros);
-		System.out.println("Bubble Sort: " +ord.exibirVetor(numeros));
+
+		int[] numeros = ord.geradorAleatorio(20, 100);
+
+		System.out.println("Vetor:     " +ord.exibirVetor(numeros));
+		int[] Bubble = ord.bubbleSort(numeros);
+		int[] Insertion = ord.insertionSort(numeros);
+		int[] Quick = ord.quickSort(numeros, 0, numeros.length-1);
+		System.out.println("Bubble:    " +ord.exibirVetor(Bubble));
+		System.out.println("Insertion: " +ord.exibirVetor(Insertion));
+		System.out.println("QuickSort: " +ord.exibirVetor(Quick));
 	}
 }
