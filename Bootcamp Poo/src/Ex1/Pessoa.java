60b50cee8a8bc905ac4a4be418ccfb8d693807e2
BryanWille <84272231+BryanWille@users.noreply.github.com>
2021-09-22 18:07:28 -0300

    Classe Pessoa

---
+++ Bootcamp Poo/src/Ex1/Pessoa.java
@@ -0,0 +1,41 @@
+package Ex1;
+
+import java.util.Date;
+
+public class Pessoa {
+    private String nome;
+    private Date nasc;
+    private float altura;
+
+    public Pessoa(String n, int y, int m, int d, float alt){
+        this.setAltura(alt);
+        this.setNasc(y, m, d);
+        this.setNome(n);
+    }
+
+    public String idade(){
+        return String.valueOf(nasc.getYear() - 2002);
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public Date getNasc(){
+        return this.nasc;
+    }
+    public void setNasc(int year, int month, int date){
+        Date nasc = new Date(year, month, date);
+        this.nasc = nasc;
+    }
+
+    public float getAltura(){
+        return this.altura;
+    }
+    public void setAltura(float altura){
+        this.altura = altura;
+    }
+}
