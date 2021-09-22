55247dda25d8587ee79c2dccd171e1672550225b
BryanWille <84272231+BryanWille@users.noreply.github.com>
2021-09-22 17:37:15 -0300

    Consertando Prova1

--- AED2/src/Prova1/prova1.java
+++ AED2/src/Prova1/prova1.java
@@ -2,21 +2,26 @@ package Prova1;
 
 public class prova1 {
     public static void main(String[] args){
-        System.out.println(potenciaIterativa(5, 4));
+        System.out.println(MDCRecursivo(12, 18));
+        System.out.println(potenciaIterativa(2, 5));
     }
-    public static void MDCRecursivo(int a, int b){
-        int n = 0;
-        if( a % n > 0 || b % n > 0){
-            System.out.println("Teste");
+    public static int MDCRecursivo(int a, int b){
+        if(b < 1) {
+            return a;
+        } else {
+            return MDCRecursivo(b, a % b);
         }
     }
 
     public static int potenciaIterativa(int base, int expoente){
         int num = base;
-        for(int cont = 1; cont <= expoente; cont++){
+        for(int cont = 1; cont < expoente; cont++){
             num *= base;
-            System.out.println((num *= base));
         }
         return num;
+    }
+
+    public static boolean primoRecursivo(int num){
+        return true;
     }
 }
