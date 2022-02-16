a865ad9f965812e48413d3de1f0fb2c75f5cbe7e
Bryan WIlle <bryanelliw@gmail.com>
2022-02-16 14:17:43 -0300

    Começando ex1

---
+++ aed3/src/Ex1/Funcionario.java
@@ -0,0 +1,52 @@
+package Ex1;
+
+public class Funcionario {
+    private String nome;
+    private float salario;
+
+
+    public Funcionario(String nome, float salario){
+        this.setNome(nome);
+        this.setSalario(salario);
+    }
+
+    public void adicionar(Funcionario func){
+
+    }
+
+    public Funcionario buscar(int posicao){
+
+    }
+
+    public void remover(int posicao){
+
+    }
+
+    public boolean contem(Funcionario func){
+
+    }
+
+    public int tamanho(){
+
+    }
+
+    public void imprimeLista(){
+
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+}
