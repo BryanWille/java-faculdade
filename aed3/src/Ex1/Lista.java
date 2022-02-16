50ab0d2d75e5b5fb2ea78d8f0b9b1d0a35e89528
bryan.braga <bryanelliw@gmail.com>
2022-02-16 16:25:07 -0300

    Falta somente metodo de organização de string

--- aed3/src/Ex1/Lista.java
+++ aed3/src/Ex1/Lista.java
@@ -1,36 +1,68 @@
 package Ex1;
 
-public class Lista{
+public class Lista {
     private Funcionario[] funcionarios = new Funcionario[100];
     private int tamanho;
 
-    public void adicionar(Funcionario func){
+    public void adicionar(Funcionario func) {
         funcionarios[tamanho] = func;
         this.tamanho++;
 
     }
 
-    public Funcionario buscar(int posicao){
-        if(this.tamanho < posicao){
-            System.out.println("Erro posição não encontrada!");
+    public Funcionario buscar(int posicao) {
+        Funcionario buscado = null;
+        if (this.tamanho < posicao || posicao > 99) {
+            System.out.println("Erro: posição não encontrada!");
         } else {
             System.out.println("Funcionário encontrado");
+            buscado = funcionarios[posicao];
+            System.out.println("O Seu funcionário se chama: " + buscado.getNome() + " e tem um salário de: R$" + buscado.getSalario());
+        }
+        return buscado;
+    }
 
+    public void remover(int posicao) {
+        if (funcionarios[posicao] == null) {
+            System.out.println("Erro: nenhum funcionário cadastrado ainda!");
+        } else {
+            funcionarios[posicao] = null;
+            int cont = this.tamanho;
+            for (int i = 0; i < cont; i++) {
+                if (funcionarios[i] == null) {
+                    Funcionario clone = funcionarios[i + 1];
+                    funcionarios[i] = clone;
+                    funcionarios[i + 1] = null;
+                    this.tamanho--;
+                }
+            }
+            this.tamanho++;
         }
     }
 
-    public void remover(int posicao){
+    public boolean contem(String nome) {
+        boolean contem = false;
+        for (int i = 0; i <= this.tamanho; i++) {
+            if (funcionarios[i].getNome().equalsIgnoreCase(nome)) {
+                contem = true;
+                break;
+            }
+        }
+        return contem;
     }
 
-    public boolean contem(String nome){
+    public int tamanho() {
+        return this.tamanho;
     }
 
-    public int tamanho(){
+    public void imprimirLista() {
+        for (int i = 0; i < this.tamanho; i++) {
+            System.out.println("Nome: " + funcionarios[i].getNome() + "\nSalário: R$" + funcionarios[i].getSalario() + "\n-----------");
+        }
     }
 
-    public void imprimirLista(){
+    public void ordemAlfabetica(Funcionario[] funcionarios){
+
     }
-
-
 
 }
--- aed3/src/Ex1/Main.java
+++ aed3/src/Ex1/Main.java
@@ -2,6 +2,29 @@ package Ex1;
 
 public class Main {
     public static void main(String[] args){
+        Funcionario Bryan = new Funcionario("Bryan Wille", 923.3f);
+        Funcionario Henrique = new Funcionario("Henrique Augusto", 2913f);
+        Funcionario Peter = new Funcionario("Peter Parker", 0f);
+        Funcionario Ash = new Funcionario("Ash Ketchion", 230f);
+        Funcionario Quinn = new Funcionario("Quinn Valor", 3170f);
+
+
+        Lista listaFuncionario = new Lista();
+        listaFuncionario.adicionar(Bryan);
+        listaFuncionario.adicionar(Henrique);
+        listaFuncionario.adicionar(Peter);
+        listaFuncionario.adicionar(Ash);
+        listaFuncionario.adicionar(Quinn);
+
+        listaFuncionario.imprimirLista();
+
+        listaFuncionario.remover(2);
+
+        System.out.println(listaFuncionario.buscar(3));
+        System.out.println(listaFuncionario.buscar(22));
+        System.out.println(listaFuncionario.contem("Bryan Wille"));
+        System.out.println(listaFuncionario.contem("Trundle"));
+
 
     }
 }
