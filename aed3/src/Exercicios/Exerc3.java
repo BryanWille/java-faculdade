141dfa033c89e471c38db96001667612c20298e9
Bryan Wille <84272231+BryanWille@users.noreply.github.com>
2022-04-03 12:27:33 -0300

    Agora entendi a aula de escrever e ler dados.

--- aed3/src/Exercicios/Exerc3.java
+++ aed3/src/Exercicios/Exerc3.java
@@ -6,18 +6,12 @@ public class Exerc3 {
     public static void main(String[] args) throws IOException {
         Reader arquivo = new FileReader("nomes.txt");
         BufferedReader texto = new BufferedReader(arquivo);
-        int contLinhas = 1, contChar = 0, whichChar = 0;
-        for (int i = 0; (whichChar = texto.read()) != -1; i++) {
-            contChar++;
-            if (whichChar == 13) {
-                contChar++;
-                whichChar = texto.read();
-                if(whichChar == 10){
-                    contChar++;
-                    contLinhas++;
-                }
-            }
-
+        int contLinhas = 0, contChar = 0;
+        String linha = texto.readLine();
+        while (linha != null){
+            contLinhas++;
+            contChar += linha.length();
+            linha = texto.readLine();
         }
         System.out.println("Quantidade de caracteres: " + contChar);
         System.out.println("Quantidade de linhas: " +contLinhas);
---
+++ aed3/src/Exercicios/Exerc4.java
@@ -0,0 +1,35 @@
+package Exercicios;
+
+import java.io.*;
+
+public class Exerc4 {
+    public static void main(String[] args) throws IOException{
+        double[] valores = {1.23, 1.31, 21.41, 123.45, 123.623};
+        writeData(valores, "coxinha");
+        double[] val = readData("coxinha");
+        for(double numeros : val ){
+            System.out.println(numeros);
+        }
+
+    }
+
+    public static void writeData(double[] data, String nome_arq) throws IOException{
+        FileOutputStream arquivo = new FileOutputStream(nome_arq);
+        DataOutputStream escrever = new DataOutputStream(arquivo);
+        escrever.writeInt(data.length);
+        for(int i = 0;i < data.length; i++){
+            escrever.writeDouble(data[i]);
+        }
+    }
+
+    public static double[] readData (String nome_arq) throws IOException{
+        FileInputStream arquivo = new FileInputStream(nome_arq);
+        DataInputStream ler = new DataInputStream(arquivo);
+        double[] valores = new double[ler.readInt()];
+        for(int i = 0; i < valores.length; i++){
+            valores[i] = ler.readDouble();
+        }
+        return valores;
+    }
+
+}
