86e12a19a647da2bddbe972ead5b284ac6cdb8a4
Bryan Wille <84272231+BryanWille@users.noreply.github.com>
2022-05-03 13:45:30 -0300

    Ainda fazendo o Conversor de Dias do java.

--- cdp/src/ConversorDias.java
+++ cdp/src/ConversorDias.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class ConversorDias {
     public static void main(String[] args) {
-        int tempoDias = 370;
-        int ano = (tempoDias / 365);
-        int dias = tempoDias % 30;
-        int meses = ((tempoDias - dias) - ano * 365) / 30;
-        System.out.printf("%d ano(s)\n%d mes(es)\n%d dia(s)", ano, meses, dias);
+        Scanner keyb = new Scanner(System.in);
+        int tempoDias = keyb.nextInt();
+        double ano = tempoDias / 365f;
+        double meses = ((ano - Math.floor(ano)) * 365) / 30f;
+        double dias = (meses - Math.floor(meses)) * 30;
+        System.out.printf("%.0f ano(s)\n%.0f mes(es)\n%.0f dia(s)", ano, meses, dias);
+        keyb.close();
     }
 }
