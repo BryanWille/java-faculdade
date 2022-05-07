140ad139912d1d165ece6c1f2c0ed5dee4986c74
Bryan Wille <84272231+BryanWille@users.noreply.github.com>
2022-05-07 20:56:25 -0300

    Competicao feito com sucesso

--- cdp/src/Competicao.java
+++ cdp/src/Competicao.java
@@ -5,54 +5,50 @@ public class Competicao {
         Scanner keyb = new Scanner(System.in);
         String quantidadeGeral;
 
-        do{
+        do {
             quantidadeGeral = keyb.nextLine();
-            if(!quantidadeGeral.equalsIgnoreCase("0 0")){
-                boolean zerou = false, fechou = false, nenhumAcertou = false;
-                int menor = 0, maior = 0, cont, checklist = 0, todoProb = 0;
+            if (!quantidadeGeral.equalsIgnoreCase("0 0")) {
+                boolean zerou = false, fechou = false, todosAcertaramUma = false;
+                int cont, checklist = 0, todoProb = 0;
 
                 int quantPlayer = Integer.parseInt(quantidadeGeral.substring(0, quantidadeGeral.indexOf(" ")));
-                int quantQuest = Integer.parseInt(quantidadeGeral.substring(quantidadeGeral.indexOf(" ")+1));
+                int quantQuest = Integer.parseInt(quantidadeGeral.substring(quantidadeGeral.indexOf(" ") + 1));
                 int[][] jogador = new int[quantPlayer][quantQuest];
-                for(int i = 0; i < quantPlayer; i++){
+
+                for (int i = 0; i < quantPlayer; i++) {
                     cont = 0;
-                    String[] jogo = keyb.nextLine().split(" ");
-                    for(int j = 0; j < quantQuest; j++){
+                    String[] jogo = keyb.nextLine().split(" "); // jogadas
+
+                    for (int j = 0; j < quantQuest; j++) {
                         jogador[i][j] = Integer.parseInt(jogo[j]);
                         cont += jogador[i][j];
-                        if(maior < cont){
-                            maior = cont;
-                        } else if (menor > cont){
-                            menor = cont;
-                        }
                     }
-                    if (cont == 0){
+                    if (cont == 0) {
                         zerou = true;
-                    } else if (cont == quantQuest){
+                    } else if (cont == quantQuest) {
                         fechou = true;
                     }
                 }
 
-
-                for(int i = 0; i < quantQuest; i++){
+                for (int i = 0; i < quantQuest; i++) {
                     int somaColuna = 0;
-                    for(int j = 0; j < quantPlayer; j++){
+                    for (int j = 0; j < quantPlayer; j++) {
                         somaColuna += jogador[j][i];
                     }
-                    if(somaColuna > 0){
-                        nenhumAcertou = true;
+                    if (somaColuna > 0) {
+                        if (somaColuna == quantPlayer) todosAcertaramUma = true;
                         todoProb++;
                     }
                 }
 
-                if(!zerou) checklist++;
-                if(!fechou) checklist++;
-                if(nenhumAcertou) checklist++;
-                if(todoProb == quantQuest) checklist++;
-                System.out.print(checklist +"\n");
+                if (!zerou) checklist++;
+                if (!fechou) checklist++;
+                if (!todosAcertaramUma) checklist++;
+                if (todoProb == quantQuest) checklist++;
+
+                System.out.print(checklist + "\n");
 
             }
-
         } while (!quantidadeGeral.equals("0 0"));
 
     }
